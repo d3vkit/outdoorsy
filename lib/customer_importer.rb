2d77160d5ad726a879b0bb4241f77c8e98eef764
Tom <323595+d3vkit@users.noreply.github.com>
2022-03-26 16:11:22 -0700

    Add more tests (#5)

--- lib/customer_importer.rb
+++ lib/customer_importer.rb
@@ -19,5 +19,7 @@ class CustomerImporter
   def validate_input(file_path, file)
     raise ArgumentError, 'Must provide either file path or file!' if file_path.blank? && file.nil?
     raise ArgumentError, 'Must provide only file path or file!' if file_path.present? && file.present?
+    raise ArgumentError, 'File must exist at given file path!' if file_path.present? && !File.exist?(file_path)
+    raise ArgumentError, 'File must be a real file!' if file.present? && !File.file?(file)
   end
 end
