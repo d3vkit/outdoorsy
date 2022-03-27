b5d5734bf31c774e9008a79cef14eeddb0de8a24
Tom Brooks <d3vkit@gmail.com>
2022-03-26 19:03:49 -0700

    [5] Show customer data

---
+++ README.md
@@ -0,0 +1,9 @@
+# Outdoor.sy Customer List Tool
+
+## The customer list tool provides a way to quickly view and sort customer data using either
+a comma-separated or pipe-seperated list of customers.
+
+# Use
+From a ruby console:
+
+`ruby customer_list.rb`
--- lib/customers_list.rb
+++ lib/customers_list.rb
@@ -10,7 +10,23 @@ class CustomersList
     @customers = Array.wrap(customers)
   end
 
-  def show(sort = :name)
+  def show
+    print_headers
 
+    customers.each do |customer|
+      print "\n"
+      print "#{customer.name} | #{customer.email} | #{customer.vehicle_type} | #{customer.vehicle_name} |"
+      print " #{customer.vehicle_length}"
+    end
+  end
+
+  private
+
+  def print_headers
+    print headers.join(' | ')
+  end
+
+  def headers
+    ['Name', 'Email', 'Vehicle Type', 'Vehicle Name', 'Vehicle Length']
   end
 end
