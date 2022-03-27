b8e2445aad79de99091890694402e7f8b94215b9
Tom Brooks <d3vkit@gmail.com>
2022-03-26 18:44:08 -0700

    WIP

--- spec/lib/customer_importer_spec.rb
+++ spec/lib/customer_importer_spec.rb
@@ -1,30 +1,12 @@
 # frozen_string_literal: true
 
-require 'fileutils'
-
 require_relative '../../lib/customer_importer'
-
-def read_test_file
-  File.open('test.txt')
-end
-
-def write_test_file
-  return File.open('test.txt') if File.exist?('test.txt')
-
-  File.write('test.txt', 'test')
-end
-
-def delete_test_file
-  File.delete('test.txt') if File.exist?('test.txt')
-end
 
 RSpec.describe CustomerImporter do
   subject(:customer_importer) { described_class.new(file_path:, col_sep:) }
 
   let(:file_path) { 'spec/fixtures/commas.txt' }
   let(:col_sep)   { ',' }
-
-  after { delete_test_file }
 
   describe '#parse' do
     subject(:parse) { customer_importer.parse }
